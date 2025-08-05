f00f6e4925eb7e2d2c49e038a7f89570e8cf3fcc
ravi-innovate <maliravikumar568@gmail.com>
2025-08-06 00:39:56 +0530

    bug fix

--- src/app/tech/components/ImageSubstitute.tsx
+++ src/app/tech/components/ImageSubstitute.tsx
@@ -1,7 +1,7 @@
 export default function ImageSubstitute({ text }: { text: string }) {
     return <>
         <div className="flex justify-center items-center text-2xl font-bold bg-center rounded-lg mb-4 text-center p-3" style={{
-            backgroundImage: "url('images/tech-basic-image.png')", backgroundSize: "100% 100%", backgroundPosition: "center", minHeight: "165px", color: "#0a000d",
+            backgroundImage: "url('images/blog-basic-image.png')", backgroundSize: "100% 100%", backgroundPosition: "center", minHeight: "165px", color: "#0a000d",
             textShadow: "0px 1px 2px #ffdd06, -1px 1px 2px #00e3e8, -2px -1px 2px #ff4273"
         }}>
             {text}
